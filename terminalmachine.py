8fe9fbb92335b3e93bb996d4808d1dac5111b0df
Jonah HH <aperson314156@gmail.com>
2023-05-14 14:56:44 -0700

    Message describing the change

--- terminalmachine.py
+++ terminalmachine.py
@@ -1,4 +1,6 @@
-# Asks for level wfidth and height
+decode = {0: "O", 1: "▷", 2: "◁", 3: "↻"}
+
+# Asks for level width and height
 
 import numpy as np
 width = int(input("Level Width: "))
@@ -6,15 +8,8 @@ height = int(input("Level Height: "))
 
 # Creates blank grid 
 
-# grid = np.zeros((width, height))
-
-grid = []
-for i in range(width):
-    row = []
-    for j in range(height):
-        row.append(0)
-    grid.append(row)
-
+grid = np.zeros((width, height), dtype=int)
+    
 # Creates function that outputs each coordinate of cells matching a cell ID
 
 def search(cell):
@@ -37,21 +32,24 @@ def show():
     for row in grid:
         line = ""
         for square in row:
-            line += str(square)
+            line += decode[square]
         print(line)
 
 while(True):
 
     # Asks for row and column if input is a number
+    addition = input("What cell to add? ('help' for options)")
+    
+    try:
 
-    addition = input("What cell to add? Type TICK for simulation. ")
-    if addition != "TICK":
+        # I'm hoping that test will just skip right to the tick function
+        test = int(addition)
         y = height - int(input("Row? "))
         x = int(input("Column? ")) - 1
         grid[y][x] = int(addition)
-    if addition == "TICK":
+    except ValueError:
 
-    # Deletes the original position of 1's and 2's and creates new ones either at the spot next to them or wrapped around the grid
+        # Deletes the original position of 1's and 2's and creates new ones either at the spot next to them or wrapped around the grid
 
         coord = search(1)
         xcoords = coord[0]
@@ -110,7 +108,7 @@ while(True):
     # There is no code for the flipper (3 ⮌) because the flip is built into left (1 ▷) and right (2 ◁)
     # This does make the code quite asymmetric, but there are always ambiguous situations like ▷ ◁
 
-    show()
+        show()
 
 
 # △▷▽◁ 
